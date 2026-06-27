6dc733351199f9467d6e4aad7dc6ca3f30316899
muzaedoen <90777694+muzaedoen@users.noreply.github.com>
2026-06-27 16:52:21 +0700

    Add product data to products.js

---
+++ src/data/products.js
@@ -0,0 +1,30 @@
+export const products = [
+  {
+    id: "produk-01",
+    no: "01",
+    name: "Kemeja Flanel Premium Pria",
+    affiliateUrl: "https://shopee.co.id/universal-link-affiliate-kamu-1",
+    ogTitle: "Kemeja Flanel Premium Murah di Shopee",
+    ogDescription: "Diskon cuci gudang kemeja flanel premium cuma hari ini. Yuk cek promonya!",
+    ogImage: "https://images.unsplash.com/photo-1598033129183-c4f50c736f10?w=600"
+  },
+  {
+    id: "produk-02",
+    no: "02",
+    name: "Sepatu Sneakers Running Casual",
+    affiliateUrl: "https://shopee.co.id/universal-link-affiliate-kamu-2",
+    ogTitle: "Sepatu Sneakers Running Casual - Diskon 50%",
+    ogDescription: "Rekomendasi sepatu running empuk dan trendi. Klik untuk klaim voucher gratis ongkir.",
+    ogImage: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?w=600"
+  },
+  {
+    id: "produk-03",
+    no: "03",
+    name: "Jam Tangan Chronograph Waterproof",
+    affiliateUrl: "https://shopee.co.id/universal-link-affiliate-kamu-3",
+    ogTitle: "Jam Tangan Mewah Pria Anti Air Original",
+    ogDescription: "Rekomendasi jam tangan analog waterproof elegan. Produk terlaris minggu ini.",
+    ogImage: "https://images.unsplash.com/photo-1523275335684-37898b6baf30?w=600"
+  }
+];
+
