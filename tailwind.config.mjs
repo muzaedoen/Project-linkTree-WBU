1de2e06680d39816bf25ea9ca0dd9129670cc8c5
muzaedoen <90777694+muzaedoen@users.noreply.github.com>
2026-06-27 20:05:43 +0700

    Create tailwind.config.mjs

---
+++ tailwind.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // Baris di bawah ini yang memberi tahu Tailwind untuk memproses CSS di semua file Astro kamu
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
