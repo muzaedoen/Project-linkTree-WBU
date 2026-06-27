e993502d5549c003d61291b4491433c8c4b0f84e
muzaedoen <90777694+muzaedoen@users.noreply.github.com>
2026-06-27 19:55:02 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,7 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel'; // <--- Pastikan ini ada jika pakai SSR
 
+// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server', // <--- Opsional: Ubah ke server jika datanya ingin real-time dari database nanti
-  adapter: vercel(),
 });
-
